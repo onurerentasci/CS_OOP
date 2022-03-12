f2ed42d18dba5a23aab3d78ab7c99ad114d3aa16
Onur Eren TAŞCI <onurerentasci@outlook.com>
2022-03-12 04:06:29 +0300

    CS_OOP

---
+++ OOP.AddAClass/Employee.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP.AddAClass
+{
+    class Employee
+    {
+        private string _firstName; // belirtmesek bile bu ifade private
+        private string _lastName;
+        public string FirstName
+        {
+            set
+            {
+                _firstName = value; // artık employee içerisinden new ile yeni nesne üretildiğinde bu nesnenin firstname propu dışardan gelen bir değere yazılabilir
+            }
+            get
+            {
+                return _firstName; // employee üzerinde okuma yazma yapılabilir
+            }
+        }
+
+        public string LastName 
+        { 
+            // Lambda formatı // Expression-Bodied Property Accessors
+            get => _lastName; 
+            set => _lastName = value; 
+        }
+
+        // Auto-implemented property
+        public int Age{ get; set; }
+
+        //Constructor
+        public Employee()
+        {
+            System.Console.WriteLine("Yapılandırıcı metot çalıştı");
+        }
+
+        // Overloading
+        public Employee(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName; // this employeeyi temsil eder
+            this.LastName = lastName;
+            this.Age = age;
+        }
+    }
+
+} 
---
+++ OOP.AddAClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OOP.AddAClass
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            employee.FirstName = "Onur Eren";
+            employee.LastName = "Taşcı";
+            employee.Age = 20;
+
+            Console.WriteLine(employee.FirstName);
+            Console.WriteLine(employee.LastName);
+            Console.WriteLine(employee.Age);
+
+            Console.WriteLine("------------------");
+
+            Employee employee2 = new Employee("Kerem", "Taşcı", 16);
+
+            Console.WriteLine(employee2.FirstName);
+            Console.WriteLine(employee2.LastName);
+            Console.WriteLine(employee2.Age);
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+}
